7e6fe448301d5f780c31842709e3b3007baf4f3a
hsd <1426482738@qq.com>
2020-01-06 11:49:32 +0800

    [IMP] 将拦截器的 any 修改为 or ，以减少花费时间

--- project/interceptor/before_req.py
+++ project/interceptor/before_req.py
@@ -23,9 +23,10 @@ def app_before_request():
         jwt_str = request.headers.get('Authorization')
         if not jwt_str:
             return comm_ret(code=resp_code.USER_NO_LOGIN, msg="请登录")
-    elif any([
+    elif (  # 使用 or 判断是否需要拦截
         ('/user/' in req_path and 'login' not in req_path)
-    ]):
+        # or ('file' in req_path)
+    ):
         # 拦截器进行用户认证，认证完成后将相关数据放到 flask 当前应用环境的通用变量 g 中，
         # 后续模块可以通过 g 对象来获取设置在其中的数据，
         # 当数据不存在时出现 '_AppCtxGlobals' object has no attribute 'xxx' 异常
