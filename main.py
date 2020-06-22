82a4586e712fcb370abd088775375b80f176c2f6
hsd <1426482738@qq.com>
2020-06-22 15:38:42 +0800

    [IMP] 修改启动封装

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-
 '''
 @File :  main.py
 @Desc :  项目启动模块
@@ -12,13 +11,16 @@
 from project.app import app
 from project.config.sys_config import app_run_conf, isFormalSystem
 
-if __name__ == '__main__':
+
+def main_run():
     debug_value = app_run_conf['DEBUG']
     # 判断是否为生产系统，若不是则开启 debug 功能
     if isFormalSystem:
         debug_value = False
-    app.run(
-        host=app_run_conf['HOST'],
-        port=app_run_conf['PORT'],
-        debug=debug_value
-    )
+    app.run(host=app_run_conf['HOST'],
+            port=app_run_conf['PORT'],
+            debug=debug_value)
+
+
+if __name__ == '__main__':
+    main_run()
